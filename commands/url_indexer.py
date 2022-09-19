c5fcf806e893863a5c9814f59d07ed271456e068
scuml <stephen@volume.com>
2022-09-19 17:10:20 -0400

    Adds logger

--- commands/url_indexer.py
+++ commands/url_indexer.py
@@ -1,25 +1,28 @@
 import sublime, sublime_plugin
 
+from collections import namedtuple
 from pathlib import Path
 from glob import iglob
 import json
 import subprocess
 
-class URLIndex:
-    def __init__(self, *args, **kwargs):
-        self.autocomplete = []
-        self.index = {}
+from .utils.common import Logger
+
+logger = Logger(__name__)
+
+IndexEntry = namedtuple('IndexEntry', ['full_method', 'url', 'method_name', 'kwargs'])
+
+class URLIndex(dict):
 
     @classmethod
     def init_with_index(cls, index):
         self = cls()
-        self.autocomplete = {i[2]: i for i in index if i[2] is not None}
-        self.index = index
+        self = {i[2].lower(): IndexEntry(*i) for i in index if i[2] is not None}
         return self
+
 
 class DjangoUrlsIndexer(sublime_plugin.WindowCommand):
     """A window command to index django url files."""
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -30,20 +33,6 @@ class DjangoUrlsIndexer(sublime_plugin.WindowCommand):
         self.python_interpreter = None
 
 
-    def run(self):
-        """Runs the indexer command.
-        Once this starts running, it will not listen to other calls until the
-        index is finished running.
-        """
-        print("DJI RUN")
-        if self._is_running:
-            return
-        self._is_running = True
-        self.window.active_view().set_status('DjangoUrls index', 'starting')
-        self._total_indexed = 0
-        print("Index start")
-        return ["a"]
-
     def clear_index(self):
         self.index = {}
 
@@ -51,102 +40,141 @@ class DjangoUrlsIndexer(sublime_plugin.WindowCommand):
         self.window_index[window] = URLIndex.init_with_index(index)
 
     def index_project_folders(self, window, project_folders):
-        print(project_folders)
+        if self._is_running:
+            return
+        self._is_running = True
+
+        logger.debug('Indexing project data...')
         managepy = None
         for folder in project_folders:
-            path = Path(folder.get('path'))
-            exclude = folder.get('folder_exclude_patterns')
-            managepy = path.glob('**/manage.py')
+            path_str = folder.get("path")
+            if not path_str:
+                continue
+            path = Path(path_str)
+            exclude = folder.get("folder_exclude_patterns")
+            managepy = path.glob("**/manage.py")
             if not managepy:
                 continue
 
         if not managepy:
-            print("No Manage.py found.")
+            logger.info('No manage.py found in project.  URL Index stopped.')
             return
-        managepy = next(managepy)
-        print(managepy)
 
-        this_path = Path( __file__ ).parent.absolute()
+        managepy = next(managepy)
+
+        this_path = Path(__file__).parent.absolute() / 'utils'
         cmd = f'import sys; sys.path.append("{this_path}"); from url_extract import run; run()'
-        result = subprocess.run([self.python_interpreter, managepy, "shell", "-c", cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(result.stderr)
+        result = subprocess.run(
+            [self.python_interpreter, managepy, "shell", "-c", cmd],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if result.stderr:
+            logger.error(result.stderr)
+
         if not result.stdout:
+            logger.warning('No output from url_extract script.')
             return
 
         self.set_window_index(window, json.loads(result.stdout))
 
+        logger.debug('Indexing complete.')
+
+        self._is_running = False
+
     def get_index_for_window(self, window):
         return self.window_index.get(window)
+
 
 djurls_indexer = DjangoUrlsIndexer(sublime.active_window())
 
 
 class DjangoUrlsPlugin(sublime_plugin.EventListener):
     def on_activated(self, view):
-        #return
         # for testing
         self.on_load_project_async(view.window())
 
     def on_load_project_async(self, window):
-        print("on load")
         project_data = window.project_data()
 
-        print(window, "just got loaded", project_data)
-        python_interpreter = project_data.get('settings', {}).get('python_interpreter')
-        project_folders = project_data.get('folders')
+        if not project_data:
+            return
+
+        window.active_view().set_status(
+            "djaneiro", "Reloading URL index for Djaneiro..."
+        )
+
+        python_interpreter = project_data.get("settings", {}).get("python_interpreter")
+        project_folders = project_data.get("folders")
+
         if not project_folders or not python_interpreter:
             return
+
         djurls_indexer.python_interpreter = Path(python_interpreter)
         djurls_indexer.index_project_folders(window, project_folders)
-
-        # Call Indexer if view doesn't have a url index
-        #if not hasattr(view, "djurlindex"):
-        #    view.djurlindex = djurls_indexer.run()
-        #    print("index updated")
+        window.active_view().erase_status("djaneiro")
 
     def on_query_completions(self, view, prefix, locations):
-        print("qc11")
-        if not view.match_selector(locations[0], "text.url-name.django"):
+        format_method = None
+
+        if view.match_selector(locations[0], "meta.function_call.python"):
+            LENGTH_OF_REVERSE = len(prefix) + 8
+            region = sublime.Region(
+                locations[0] - LENGTH_OF_REVERSE, locations[0] - len(prefix)
+            )
+            if view.substr(region) == "reverse(":
+                format_method = self.completions_for_reverse
+
+        elif view.match_selector(locations[0], "text.url-name.django"):
+            format_method = self.completions_for_url_tag
+
+        if not format_method:
             return []
 
-        print("qc", view.window(), dir(view.window()))
+
 
         index = djurls_indexer.get_index_for_window(view.window())
         if not index:
-            return
+            return []
 
         prefix = prefix.lower()
 
         out = []
-        for view_name, payload in index.autocomplete.items():
-            if view_name.lower().startswith(prefix):
-                params = ''
-                if payload[3]:
-                    params = " " + " ".join([f'${{{i}:{p}}}' for i, p in enumerate(payload[3], 1)])
-                out.append((
-                f'{view_name}\t{payload[0]}', f"'{view_name}'" + params
-            ))
-        print(out)
+        for view_name, index_entry in index.items():
+            if view_name.startswith(prefix):
+                out.append(format_method(index_entry))
 
-        return (out, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+        return (
+            out,
+            sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS,
+        )
 
-#     def on_pre_save(self, view):
-#         print(view.file_name(), "is about to be saved")
+    def completions_for_reverse(self, index_entry):
 
-#     def on_post_save(self, view):
-#         print(view.file_name(), "just got saved")
+        params = ""
+        if index_entry.kwargs:
+            params = ", ".join(
+                [f"'{p}': ${{{i}:{p}}}" for i, p in enumerate(index_entry.kwargs, 1)]
+            )
+            params = ", kwargs={" + params + "}"
 
-#     def on_new(self, view):
-#         print("new file")
+        return f"{index_entry.method_name}\t{index_entry.full_method}", f"'{index_entry.method_name}'" + params
 
-#     def on_modified(self, view):
-#         print(view.file_name(), "modified")
+    def completions_for_url_tag(self, index_entry):
+        params = ""
+        if index_entry.kwargs:
+            params = " " + " ".join(
+                [f"${{{i}:{p}}}" for i, p in enumerate(index_entry.kwargs, 1)]
+            )
+        return f"{index_entry.method_name}\t{index_entry.full_method}", f"'{index_entry.method_name}'" + params
 
-#     def on_activated(self, view):
-#         print(view.file_name(), "is now the active view")
 
-#     def on_close(self, view):
-#         print(view.file_name(), "is no more")
+    def on_post_save(self, view):
+        if not view.match_selector(0, "source.python"):
+            return
 
-#     def on_clone(self, view):
-#         print(view.file_name(), "just got cloned")
+        # look for the variable urlpatterns
+        if view.find("urlpatterns", 0).empty():
+            return
+
+        self.on_load_project_async(view.window())
